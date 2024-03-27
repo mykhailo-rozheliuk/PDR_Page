79134c55870d87b6b0de013643d964ea6c5e0dd6
mike <rozheluikmisha@gmail.com>
2024-03-27 12:29:06 +0200

    chore: add eslint and prettier to the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,57 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  plugins: [
+    "react",
+    "react-hooks",
+    "react-refresh",
+    "@typescript-eslint",
+    "jsx-a11y",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-}
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    indent: "off",
+    "react/prop-types": "off",
+    semi: ["error", "always"],
+    "react/display-name": "off",
+    quotes: ["error", "single"],
+    "react/jsx-indent": ["error", 2],
+    "react/react-in-jsx-scope": "off",
+    "linebreak-style": ["error", "unix"],
+    "react/jsx-indent-props": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+import sass from 'sass';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass
+      }
+    }
+  }
+});
